d606643125e7d8e61942a7f2541d621d0b0e5d50
AayaswamiLakshmi <119338765+AayaswamiLakshmi@users.noreply.github.com>
2026-01-20 18:58:19 +0530

    Added files (#1)

---
+++ src/main/java/com/khadri/orm/StudentCRUD.java
@@ -0,0 +1,22 @@
+package com.khadri.orm;
+
+public class StudentCRUD {
+
+    public static void insert(Student student) {
+        
+    }
+
+    public static Student find(int id) {
+        return null; 
+    }
+
+
+    public static void update(Student student) {
+        
+    }
+
+    public static void delete(int id) {
+       
+    }
+}
+
